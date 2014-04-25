433d49274051037082de1c5ae84b1cd5a64d3755
suicider <suicider@gmx.de>
2014-04-25 14:09:02 +0200

    Added Logger

--- heartbeat.sh
+++ heartbeat.sh
@@ -4,8 +4,8 @@ IP=$(uci get network.wireless0.ipaddr)
 NET=$(uci get profile_erfurt.profile.mesh_network)
 OLSR=/var/etc/hosts.olsr
 SERVER=''
-#
 SUBNET=`echo $NET | cut -d/ -f1 | cut -d. -f1,2`
 HOSTS=`grep $SUBNET $OLSR | wc -l`
 
+logger -t Heartbeat Send
 /usr/bin/wget -O - $SERVER?$IP+$HOSTS &2>/dev/null
